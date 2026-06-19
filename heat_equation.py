77e62016f9178c9c2c1026817ac891f090e9e019
morgp81 <mtphil81@gmail.com>
2026-06-18 22:25:00 -0400

    comments

--- heat_equation.py
+++ heat_equation.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 def heat_equation(u, sigma, dx):
-    dudt = np.zeros_like(u)
+    dudt = np.zeros_like(u) #initialize the array for the derivatives
 
     # boundary conditions
     dudt[0] = 0
     dudt[-1] = 0
 
-    for i in range(1, len(u)-1):
+    for i in range(1, len(u)-1): #compute each point's derivative using the second spatial derivative
         dudt[i] = sigma * (
             u[i-1] - 2*u[i] + u[i+1]
         ) / dx**2
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ N = 100  # number of spatial points
 delta_x = 1/N  # spatial step size
 sigma = 0.0001  # thermal diffusivity
 
-steps = 1000  # number of time steps
+steps = 5000  # number of time steps
 dt = 0.1  # time step size
 
 x = np.linspace(0, 1, N+1) # initial condition: sine wave
@@ -16,11 +16,11 @@ u = np.sin(2*np.pi*x)
 
 values = [u.copy()] # store the values at each time step for plotting
 
-for t in range(steps):
+for t in range(steps): #time loop
     u=implicit_rk2(u, heat_equation, dt, sigma, delta_x)
     values.append(u.copy())
 
-values = np.array(values)
+values = np.array(values) 
 print(values)
 
 plt.imshow(values, aspect='auto', extent=[0, 1, 0, steps*dt], origin='lower')
@@ -28,6 +28,7 @@ plt.colorbar(label='Temperature')
 plt.xlabel('Position')
 plt.ylabel('Time')
 plt.title('Heat Equation Evolution')
+plt.savefig("heat_equation2d.png")
 plt.show()
 
 
